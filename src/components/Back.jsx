0b4d091e6038eda63c10cc451feb82f54cebb3db
JeanPiton <jean.orpiton@gmail.com>
2023-05-27 17:03:08 -0300

    Back button correction

--- src/components/Back.jsx
+++ src/components/Back.jsx
@@ -4,6 +4,6 @@ export default function Back(){
     const navigate = useNavigate();
 
     return(
-        <ion-icon name="arrow-back-sharp" onClick={()=>navigate(-1)} data-test="go-home-btn"></ion-icon>
+        <ion-icon name="arrow-back-sharp" onClick={()=>navigate(-1)} data-test="go-home-header-btn"></ion-icon>
     );
 }
